b5731d6dd0f1eba78ef5846d611259efa03530f6
Wlmods <111136884+Wlmods@users.noreply.github.com>
2022-08-14 21:59:18 -0300

    Update XeonCheems4.js

--- XeonCheems4.js
+++ XeonCheems4.js
@@ -2598,7 +2598,7 @@ if (isBanChat) return reply(mess.banChat)
             reply('Successfully Deleted The Vote Session In This Group')
 	    }
             break
-               case 'grupo': case 'grup': {
+               case 'group': case 'grup': {
                	if (isBan) return reply(mess.ban)	 			
 if (isBanChat) return reply(mess.banChat)
                 if (!m.isGroup) return replay(`${mess.group}`)
